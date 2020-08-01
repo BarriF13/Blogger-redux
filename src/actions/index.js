1942038951e1a1f43311eef1844847b79995adcc
Barri <info@bdesignstudio.co.uk>
2020-08-01 19:21:09 +0100

    Uderstand redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,13 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts =  async ()=>{
-
-    const res = await jsonPlaceholder.get('/posts')
+export const fetchPosts = ()=>{
+  //dispatch : we can change any data we want 
+  //getState : we can read any data we want in the whole app
+  return function(dispatch, getState) {
+    const promise = jsonPlaceholder.get('/posts')
   return  {
       type: 'FETCH_POSTS',
       payload: res
     };
-  
+  }
 };

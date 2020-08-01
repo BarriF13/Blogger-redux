3f5a50ae26d9f64be9b0876709dc08a640dd8a23
Barri <info@bdesignstudio.co.uk>
2020-08-01 19:34:13 +0100

    create async action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = ()=>{
-  //dispatch : we can change any data we want 
-  //getState : we can read any data we want in the whole app
-  return function(dispatch, getState) {
-    const promise = jsonPlaceholder.get('/posts')
-  return  {
-      type: 'FETCH_POSTS',
-      payload: res
-    };
-  }
+//export const fetchPosts = ()=>
+//dispatch : we can change any data we want 
+//getState : we can read any data we want in the whole app
+// return async function(dispatch, getState) {
+//   const response =await  jsonPlaceholder.get('/posts')
+// dispatch({ type: 'FETCH_POSTS', payload: response })
+// };
+//----------Refactor ---defining a function which returning function
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+  dispatch({ type: 'FETCH_POSTS', payload: response })
 };
+

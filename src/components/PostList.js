9eead6a4f92daa8aa51fe8473a17ef2d9fbb13b7
Barri <info@bdesignstudio.co.uk>
2020-08-11 19:11:33 +0100

    Print data in dom

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
 
 export class PostList extends Component {
 
-  componentDidMount(){
+  componentDidMount() {
     //action creator 
     this.props.fetchPosts();
   }
-  renderList(){
-    
+  //make a function just for rendering the list 
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
   }
   render() {
     console.log(this.props.posts);
     return (
-      <div className="item">
-      Post list 
+      <div className="ui relaxed divided list">
+       {this.renderList()}
       </div>
     )
   }
 }
 //passing data from redux to react with below function-- and state below will have property of post because we assign to it in reducers/index.js
 const mapStateToProps = state => {
-  return {posts: state.posts};
+  return { posts: state.posts };
 }
 export default connect(
   //null, 
   mapStateToProps,
-  {fetchPosts}
-  )(PostList);
+  { fetchPosts }
+)(PostList);
